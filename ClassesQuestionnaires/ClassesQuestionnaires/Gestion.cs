8a6077f2b44168587f82136a8aad5fa6461d0312
PouletGrill <xavierb.2006@hotmail.com>
2015-03-10 11:17:45 -0400

    une couple de nouvelle affaire
    des nouvelle form qui n'ont pas encore été placer

---
+++ ClassesQuestionnaires/ClassesQuestionnaires/Gestion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;     //Librairie Oracle
+
+namespace ClassesQuestionnaires
+{
+   public partial class Gestion : Form
+   {
+      OracleConnection connection;
+      public Gestion(OracleConnection connectionx)
+      {
+         connection = connectionx;
+         InitializeComponent();
+      }
+   }
+}
