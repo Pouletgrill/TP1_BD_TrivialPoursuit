abf88f6c39f1dcb909e1c38ec7a525ee488b055c
PouletGrill <xavierb.2006@hotmail.com>
2015-03-17 12:22:31 -0400

    Modelisation :( :gun:

--- ClassesQuestionnaires/ClassesQuestionnaires/AjouterQuestion.cs
+++ ClassesQuestionnaires/ClassesQuestionnaires/AjouterQuestion.cs
@@ -114,46 +114,45 @@ namespace ClassesQuestionnaires
 
       private void BTN_Ajouter_Modifier_Click(object sender, EventArgs e)
       {
-         if (fonction == "AJOUT")
-            Ajouter();
+         String BRep, MRep1, MRep2, MRep3;
+         if (RB_Reponse1.Checked)
+         {
+            BRep = TB_Reponse1.Text;
+            MRep1 = TB_Reponse2.Text;
+            MRep2 = TB_Reponse3.Text;
+            MRep3 = TB_Reponse4.Text;
+         }
+         else if (RB_Reponse2.Checked)
+         {
+            BRep = TB_Reponse2.Text;
+            MRep1 = TB_Reponse1.Text;
+            MRep2 = TB_Reponse3.Text;
+            MRep3 = TB_Reponse4.Text;
+         }
+         else if (RB_Reponse3.Checked)
+         {
+            BRep = TB_Reponse3.Text;
+            MRep1 = TB_Reponse1.Text;
+            MRep2 = TB_Reponse2.Text;
+            MRep3 = TB_Reponse4.Text;
+         }
          else
-            Modifier();
+         {
+            BRep = TB_Reponse4.Text;
+            MRep1 = TB_Reponse1.Text;
+            MRep2 = TB_Reponse2.Text;
+            MRep3 = TB_Reponse3.Text;
+         }
+         if (fonction == "AJOUT")
+            Ajouter(BRep, MRep1, MRep2, MRep3);
+         else
+            Modifier(ID, BRep,MRep1, MRep2, MRep3);
       }
 
-      private void Ajouter()
+      private void Ajouter(String BRep, String MRep1, String MRep2, String MRep3)
       {
          try
          {
-            String BRep, MRep1, MRep2, MRep3;
-            if (RB_Reponse1.Checked)
-            {
-               BRep = TB_Reponse1.Text;
-               MRep1 = TB_Reponse2.Text;
-               MRep2 = TB_Reponse3.Text;
-               MRep3 = TB_Reponse4.Text;
-            }
-            else if (RB_Reponse2.Checked)
-            {
-               BRep = TB_Reponse2.Text;
-               MRep1 = TB_Reponse1.Text;
-               MRep2 = TB_Reponse3.Text;
-               MRep3 = TB_Reponse4.Text;
-            }
-            else if (RB_Reponse3.Checked)
-            {
-               BRep = TB_Reponse3.Text;
-               MRep1 = TB_Reponse1.Text;
-               MRep2 = TB_Reponse2.Text;
-               MRep3 = TB_Reponse4.Text;
-            }
-            else
-            {
-               BRep = TB_Reponse4.Text;
-               MRep1 = TB_Reponse1.Text;
-               MRep2 = TB_Reponse2.Text;
-               MRep3 = TB_Reponse3.Text;
-            }
-
             OracleCommand oraAjout = new OracleCommand("PKG_GESTION", connection);
             oraAjout.CommandText = "PKG_GESTION.INSERTIONQUESTION";
             oraAjout.CommandType = CommandType.StoredProcedure;
@@ -198,9 +197,58 @@ namespace ClassesQuestionnaires
             MessageBox.Show(Oe.Message);
          }
       }
-      private void Modifier()
+      private void Modifier(String ID, String BRep, String MRep1, String MRep2, String MRep3)
       {
-         MessageBox.Show("cccc");
+         try
+         {            
+            OracleCommand oraAjout = new OracleCommand("PKG_GESTION", connection);
+            oraAjout.CommandText = "PKG_GESTION.MODIFIERQUESTION";
+            oraAjout.CommandType = CommandType.StoredProcedure;
+
+            //Déclaration des paramettres
+            OracleParameter procID = new OracleParameter("QUSTIONID", OracleDbType.Varchar2, 10);
+            procID.Direction = ParameterDirection.Input;
+            procID.Value = ID;
+            oraAjout.Parameters.Add(procID);
+
+            OracleParameter procQuestion = new OracleParameter("PQUESTION", OracleDbType.Varchar2, 250);
+            procQuestion.Direction = ParameterDirection.Input;
+            procQuestion.Value = TB_Question.Text;
+            oraAjout.Parameters.Add(procQuestion);
+
+            OracleParameter procCategorie = new OracleParameter("PCATEGORIE", OracleDbType.Varchar2, 10);
+            procCategorie.Direction = ParameterDirection.Input;
+            procCategorie.Value = CMB_Categories.SelectedItem.ToString()[0].ToString();//retourne le premier caractère du string
+            oraAjout.Parameters.Add(procCategorie);
+
+            OracleParameter procBRep = new OracleParameter("REPONSE_BONNE", OracleDbType.Varchar2, 250);
+            procBRep.Direction = ParameterDirection.Input;
+            procBRep.Value = BRep;
+            oraAjout.Parameters.Add(procBRep);
+
+            OracleParameter procMRep1 = new OracleParameter("REPONSE_MAUVAISE_1", OracleDbType.Varchar2, 250);
+            procMRep1.Direction = ParameterDirection.Input;
+            procMRep1.Value = MRep1;
+            oraAjout.Parameters.Add(procMRep1);
+
+            OracleParameter procMRep2 = new OracleParameter("REPONSE_MAUVAISE_2", OracleDbType.Varchar2, 250);
+            procMRep2.Direction = ParameterDirection.Input;
+            procMRep2.Value = MRep2;
+            oraAjout.Parameters.Add(procMRep2);
+
+            OracleParameter procMRep3 = new OracleParameter("REPONSE_MAUVAISE_3", OracleDbType.Varchar2, 250);
+            procMRep3.Direction = ParameterDirection.Input;
+            procMRep3.Value = MRep3;
+            oraAjout.Parameters.Add(procMRep3);
+
+            //Execution de la requête
+            oraAjout.ExecuteNonQuery();
+            //MessageBox.Show("Insertion réusit");
+         }
+         catch (OracleException Oe)
+         {
+            MessageBox.Show(Oe.Message);
+         }
       }
 
       private void AfficherCategorie(String ID)
@@ -277,7 +325,38 @@ namespace ClassesQuestionnaires
       }
       private void AfficherReponses(String ID)
       {
+         try
+         {
+            OracleCommand inserer = new OracleCommand("PKG_GESTION", connection);
+            inserer.CommandText = "PKG_GESTION.AFFICHERREPONSE";
+            inserer.CommandType = CommandType.StoredProcedure;
 
+            OracleParameter pResultat = new OracleParameter("PRESULTAT", OracleDbType.RefCursor);
+            pResultat.Direction = ParameterDirection.ReturnValue;
+            inserer.Parameters.Add(pResultat);
+
+            OracleParameter OraDesc = new
+            OracleParameter("QUESTIONID", OracleDbType.Varchar2, 10);
+            OraDesc.Value = ID;
+            OraDesc.Direction = ParameterDirection.Input;
+            inserer.Parameters.Add(OraDesc);
+
+            OracleDataReader reader = inserer.ExecuteReader();
+
+            reader.Read();
+            TB_Reponse1.Text = reader.GetString(0);
+            reader.Read();
+            TB_Reponse2.Text = reader.GetString(0);
+            reader.Read();
+            TB_Reponse3.Text = reader.GetString(0);
+            reader.Read();
+            TB_Reponse4.Text = reader.GetString(0);
+            RB_Reponse1.Checked = true;
+         }
+         catch (Exception se)
+         {
+            MessageBox.Show(se.Message.ToString());
+         }
       }
    }
 }
